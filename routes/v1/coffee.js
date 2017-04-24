29e788df823d30d773257a3d8b622fe3a95d3edc
d_ojima <daiking.ca2@gmail.com>
2017-04-24 18:52:53 +0900

    change drip api

--- routes/v1/coffee.js
+++ routes/v1/coffee.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
 let DripModel = require('../../models/dripModel.js');
 let TypeModel = require('../../models/typeModel.js');
 
@@ -41,16 +42,26 @@ router.get('/:id/:type', function(req, res, next) {
   });
 });
 
-router.get('/range/:id/:type', function(req, res, next) {
-  let userId = req.params.id;
-  let dripType = req.params.type;
+// drip?userid=d_ojima&type=0&since=2017-04-22&until=2017-04-24
+router.get('/', function(req, res, next) {
+  let userId = req.query.userid || null;
+  if (userId == null) {
+    res.json({
+      "message": "Request parameter is insufficient"
+    });
+  }
+  let dripType = req.query.type || 0;// default: 0(barista)
+  let sinceDay = req.query.since || moment.unix(0).format("YYYY-MM-DD");
+  let untilDay = req.query.until || moment().format("YYYY-MM-DD");
   // Set the range to select date
   // ref : https://www.quora.com/Node-js-How-do-range-query-in-mongoose
-  DripModel.count({user_id: userId, type: dripType, "date": {'$gte': new Date('3/24/2017'), '$lt': new Date('4/20/2017')}}, (err, c) => {
+  // 境界値を含む場合: $gte, $lte
+  // 境界値を含まない場合: $gt, $lt
+  DripModel.count({user_id: userId, type: dripType, "date": {'$gte': new Date(sinceDay), '$lte': new Date(untilDay)}}, (err, c) => {
     console.log(c);
     TypeModel.findOne({id: dripType}, (err, docs) => {
       console.log(docs);
-      let price = docs.price;
+      let price = docs.price || 0;
       res.json({
         "count": c,
         "price": c * price
